77994f498be381b804f96b2055e56802ff6b6725
Moktar Jama <moktarjama@gmail.com>
2017-04-01 20:14:10 -0400

    complete array and enumerable problem converston to JS

---
+++ categories/arrays.js
@@ -0,0 +1,52 @@
+//// Arrays ////
+
+
+// Write a method that returns an array of all of the pairs of indexes whose
+// elements sum to zero
+Array.prototype.twoSum = function () {
+
+};
+
+// Write a method that returns a hash containing any repeating elements as keys
+// with the values set to an array of the indexes of each repeat
+
+Array.prototype.dups = function () {
+
+};
+
+// Write a method that returns the sum of all elements in the array
+
+Array.prototype.sum = function () {
+
+};
+
+// Write a method that squares each element in the array
+
+Array.prototype.squareSelf = function () {
+
+};
+
+// Write a method that squares each element in the array, but does not modify the array
+
+Array.prototype.square = function () {
+
+};
+
+// Write a method that returns all of the unique elements in an array
+
+Array.prototype.myUniq = function () {
+
+};
+
+// Write a method that tranposes an array (see Array#transpose)
+
+Array.prototype.myTranspose = function () {
+
+};
+
+// Write a method that returns the median of elements in an array
+// If the length is even, return the average of the middle two elements
+
+Array.prototype.median = function () {
+
+};
---
+++ categories/enumerables.js
@@ -0,0 +1,63 @@
+//// Enumerables ////
+
+// Write a method that returns an array of all factors of a number
+function factors(num) {
+
+}
+
+// Write a method that doubles each element in an array
+
+function doubler(array) {
+
+}
+
+// Write a method that returns an array of the first n number of primes numbers
+
+function primes(n) {
+
+}
+
+Array.prototype.myEach = function () {
+
+};
+
+Array.prototype.mySelect = function () {
+
+};
+
+Array.prototype.myReject = function () {
+
+};
+
+Array.prototype.myAny = function () {
+
+};
+
+Array.prototype.myAll = function () {
+
+};
+
+Array.prototype.myFlatten = function () {
+
+};
+
+// Write a version of flatten that only flattens n levels of an array.
+Array.prototype.myControlledFlatten = function () {
+
+};
+
+Array.prototype.myZip = function () {
+
+};
+
+Array.prototype.myRotate = function (num = 1) {
+
+};
+
+Array.prototype.myJoin = function (str = "") {
+
+};
+
+Array.prototype.myReverse = function () {
+
+};
