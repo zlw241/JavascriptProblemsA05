4de4c35205195be7e84b3c63f4727a5ee7388850
Moktar Jama <moktarjama@gmail.com>
2017-04-01 20:22:13 -0400

    add recursion problems

---
+++ categories/recursion.js
@@ -0,0 +1,77 @@
+//// Recursion ////
+
+// Write a recursive method that returns the first "num" factorial numbers.
+function digital_root(num) {
+
+}
+
+// Write a recursive method that returns an array of first n number of factorials
+function factorials_rec(num) {
+
+}
+
+// Write a recursive method that returns an array of numbers between min and max
+function range(min, max) {
+
+}
+
+
+// Write a recursive method that returns the sum of all elements in an array
+function sum(nums) {
+
+}
+
+
+// Write a recursive method that exponentiates base ** power without using the ** method
+function exp(base, power) {
+
+}
+
+
+// Write a recursive method that deep dups an array
+Array.prototype.deepDup = function () {
+
+};
+
+
+// Write a recursive method that returns the first n number of fibonacci numbers in an array
+function fibs_rec(n) {
+
+}
+
+//I highly doubt that anything below will be on the test, but it's good practice
+
+// Write a recursive method subsets that will return all subsets of an array.
+Array.prototype.subsets = function () {
+
+};
+
+
+
+// Write a recursive method that returns all of the permutations of an array
+function permutations(array) {
+
+}
+
+
+// Write a recursive method that returns an array of the best change given a target amouunt
+function make_change(target, coins = [25, 10, 5, 1]) {
+
+}
+
+
+// Write a recursive method that takes a Fixnum and converts it to a string in a different base
+// Do not use the built-in #to_s(base) method.
+// |Base 10 (decimal)     |0   |1   |2   |3   |....|9   |10  |11  |12  |13  |14  |15  |
+// |Base 2 (binary)       |0   |1   |10  |11  |....|1001|1010|1011|1100|1101|1110|1111|
+// |Base 16 (hexadecimal) |0   |1   |2   |3   |....|9   |A   |B   |C   |D   |E   |F   |
+
+Number.prototype.stringify = function () {
+
+};
+
+
+// Write a recursive method that returns the sum of the first n even numbers
+function first_even_numbers_sum(n) {
+
+}
